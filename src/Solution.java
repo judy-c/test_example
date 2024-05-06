62aa24d4a6a2496b9b54b01b4f29f0b74720f182
朱迪 <zhudi1@xiaohongshu.com>
2024-05-06 11:39:21 +0800

    feat(gitignore)

--- src/Solution.java
+++ src/Solution.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("test code");
-        System.out.println("test code");
+        System.out.println("new test code");
         System.out.println("test code");
         System.out.println("test code");
         System.out.println("test code");
