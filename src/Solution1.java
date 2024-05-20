bbd8c6b9882c8185de28bcaa2f32569752e97060
朱迪 <zhudi1@xiaohongshu.com>
2024-05-20 23:55:26 +0800

    test

--- src/Solution1.java
+++ src/Solution1.java
@@ -2,6 +2,8 @@ import java.util.*;
 
 class Solution1 {
     public static void main(String[] args) {
-
+        String str = "正确答案";
+        String st1 = "正确答案";
+        String str2 = "正确答案";
     }
 }
